03585c2194cb7ef1ec1cbabab1e1a31a7481f672
JayKarhade <48925233+JayKarhade@users.noreply.github.com>
2021-07-15 13:34:51 +0530

    Update PSNR.py

--- PSNR.py
+++ PSNR.py
@@ -3,19 +3,23 @@ import cv2
 import numpy as np
 
 def PSNR(original, synthesis):
-	mse = np.mean((original - snythesis) ** 2)
-	if(mse == 0): # MSE is zero means no noise is present in the signal .
-				# Therefore PSNR have no importance.
-		return 100
-	max_pixel = 255.0
-	psnr = 20 * log10(max_pixel / sqrt(mse))
-	return psnr
+  mse = np.mean((original - synthesis) ** 2)
+  #print(mse)
+  max_pixel = 255.0
+  psnr = 20 * log10(max_pixel / sqrt(mse))
+  return psnr
 
 def main():
-	original = cv2.imread("original_image.png")
-	synthesis = cv2.imread("synthesized.png", 1)
-	value = PSNR(original, synthesis)
-	print(f"PSNR value is {value} dB")
+  psnr_sum = 0
+  for i in range(1,13):
+    original = cv2.imread("/content/drive/MyDrive/Neural-Point-Cloud-Rendering/data/ScanNet/scene0010_00/color/"+str(i)+"99.jpg")
+    original = cv2.resize(original,(640,480))
+    original  = np.asarray(original[120:(120+240),160:(160+320),:])
+    synthesis = cv2.imread("/content/drive/MyDrive/Neural-Point-Cloud-Rendering/ScanNet_npcr_scene0010_00/TestResultpytorch/"+"{0:0=6d}".format(i)+".png", 1)
+    value = PSNR(original, synthesis)
+    psnr_sum = psnr_sum + value
+    print(f"PSNR value is {value} dB")
+  print(f"Averaged PSNR value is {value} dB")
 	
 if __name__ == "__main__":
-	main()
+  main()
