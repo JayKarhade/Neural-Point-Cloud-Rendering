9858a29b257d9b904dbaf881eb5e62a985c27729
JayKarhade <48925233+JayKarhade@users.noreply.github.com>
2021-07-20 00:07:35 +0530

    Loss instantiated

--- loss.py
+++ loss.py
@@ -6,6 +6,8 @@ import scipy.io
 import torchvision.models as models
 import torchvision.models.vgg as vgg 
 from collections import namedtuple
+import time
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 LossOutput = namedtuple("LossOutput", ["conv1_2", "conv2_2", "conv3_2", "conv4_2","conv5_2"])
 #Perceptual Loss Network 
@@ -35,29 +37,23 @@ def compute_error(real,fake):
   fake = fake.to(device)
   return torch.mean(torch.abs(fake-real),dim=(1,2,3))
 
-#Compute VGG Loss between real and fake
-def VGG_loss(network_output,GT,reuse=False):
-    #print(GT.shape)
-    #print(network_output.shape)
-    #network_output = torch.from_numpy(network_output)
-    ##Check dataformat of network output
+class Perceptual_loss:
+  def __init__(self):
+    self.loss_network = LossNetwork().to(device)
+    
+  def calc_loss(self,network_output,GT,reuse=False):
     m = torch.tensor([0.485, 0.456, 0.406]).to(device)*255.0
     m = torch.reshape(m,(1,3,1,1)).to(device)
     std = torch.tensor([0.229, 0.224, 0.225]).to(device)*255.0
     std = torch.reshape(std,(1,3,1,1)).to(device)
     GT = GT.permute(0,3,1,2).to(device) 
-    # since we use OpenCV to read and write image, bgr to rgb.
-    #GT_RGB = torch.cat((GT[:, 2, :, :], GT[:, 1, :, :], GT[:, 0, :, :]),axis=2)
-    #network_output_RGB = torch.cat((network_output[:, 2, :,:], network_output[:, 1, :, :], network_output[:, 0, :, :]),axis=2)
-    
-    #network_output  = network_output.permute(0,3,1,2)
+
     fake = ((network_output*255.0))# - m)/std
     real = ((GT*255.0))# - m)/std
     #print('c1')
-    loss_network = LossNetwork().to(device)
     #print('c2')
-    real_l = loss_network(real.float().to(device))
-    fake_l = loss_network(fake.float().to(device))
+    real_l = self.loss_network(real.float().to(device))
+    fake_l = self.loss_network(fake.float().to(device))
     #print('c3')
     p = []
     div_nums = [1.6,2.3,1.8,2.8,12.5]##Constant used to divide losses
@@ -69,10 +65,15 @@ def VGG_loss(network_output,GT,reuse=False):
     #print("total loss",sum(p))
     return p[0],p[1],p[2],p[3],p[4],p[5],sum(p)
 
+
 if __name__ == '__main__':
   x1 = torch.rand(1,480,640,3).to(device)
   #x1 = torch.rand(1,3,480,640)  
   x2 = torch.rand(1,3,480,640).to(device)
   #print(compute_error(x1,x2))
-  z = VGG_loss(x2,x1)
+  loss_class = Perceptual_loss()
+  start = time.time()
+  z = loss_class.calc_loss(x2,x1)
+  end = time.time()
+  print("method call time",end-start)
   print(z)
